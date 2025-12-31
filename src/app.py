5698cc5127d4b42e157df1338730b556d36409c5
Justine-AH <justinehuang0126@gmail.com>
2025-12-31 23:43:14 +0800

    feat: Implement loading of resources to ui and saving

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import json
+import os
 import sys
+import tempfile
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow
@@ -13,7 +15,7 @@ class SaveEditor(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         
-        self.save = None
+        self.data = None
         
         self.q_app = q_app
 
@@ -21,7 +23,9 @@ class SaveEditor(QMainWindow, Ui_MainWindow):
         self.resize(1200, 800)
         self.statusBar().showMessage("Ready")
         
+        self.actionSave_File.setEnabled(False)
         self.actionLoad_File.triggered.connect(self.on_load_button_trigger)
+        self.actionSave_File.triggered.connect(self.on_save_button_trigger)
     
     def on_load_button_trigger(self):
         path, _ = QFileDialog.getOpenFileName(
@@ -35,13 +39,46 @@ class SaveEditor(QMainWindow, Ui_MainWindow):
             return
 
         with open(path, "r", encoding="utf-8") as f:
-            self.save = json.load(f)
+            self.data = json.load(f)
             
         self.load_data()
+        self.actionSave_File.setEnabled(True)
+        
+    def on_save_button_trigger(self):
+        path, _ = QFileDialog.getSaveFileName(
+            self,
+            "Save File",
+            "",
+            "Save Files (*.fcs)"
+        )
+
+        if not path:
+            return
+
+        dir_name = os.path.dirname(path)
+        fd, temp_path = tempfile.mkstemp(dir=dir_name)
+
+        try:
+            with os.fdopen(fd, "w", encoding="utf-8") as f:
+                json.dump(self.data, f, indent=2)
+
+            os.replace(temp_path, path)
+        except Exception:
+            os.remove(temp_path)
+            raise
 
     def load_data(self):
-        if self.save is not None:
-            self.statusBar().showMessage("Save file loaded")
+        if self.data is None:
+            return
+        
+        self.statusBar().showMessage("Save file loaded")
+        
+        player_data = self.data["PlayerSaveData"]
+        
+        self.goldSpinBox.setValue(player_data["Cash"])
+        self.supplySpinBox.setValue(player_data["Food"])
+        self.ammoSpinBox.setValue(player_data["Ammo"])
+        self.manpowerSpinBox.setValue(player_data["Manpower"])
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
